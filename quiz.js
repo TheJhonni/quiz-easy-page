9c2041b890107e90b51caa7f9002d1f0de831905
TheJhonni <davideconi91@gmail.com>
2021-11-08 18:50:49 +0100

    html + js + css

---
+++ quiz.js
@@ -0,0 +1,126 @@
+const questionsTot = [
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "What does CPU stand for?",
+      correct_answer: "Central Processing Unit",
+      incorrect_answers: [
+        "Central Process Unit",
+        "Computer Personal Unit",
+        "Central Processor Unit",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+      correct_answer: "Final",
+      incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "The logo for Snapchat is a Bell.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question:
+        "Pointers were not used in the original C programming language; they were added later on in C++.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the most preferred image format used for logos in the Wikimedia database?",
+      correct_answer: ".svg",
+      incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "In web design, what does CSS stand for?",
+      correct_answer: "Cascading Style Sheet",
+      incorrect_answers: [
+        "Counter Strike: Source",
+        "Corrective Style Sheet",
+        "Computer Style Sheet",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the code name for the mobile operating system Android 7.0?",
+      correct_answer: "Nougat",
+      incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "On Twitter, what is the character limit for a Tweet?",
+      correct_answer: "140",
+      incorrect_answers: ["120", "160", "100"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "Linux was first created as an alternative to Windows XP.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "Which programming language shares its name with an island in Indonesia?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "C", "Jakarta"],
+    },
+  ];
+
+let guestScore = 0
+
+const showQuestions = function () {
+    let container = document.getElementsByClassName("question-container");
+    for (let i=0; i<questionsTot.length; i++) {
+        let questionshown = questionsTot[i];
+        newdiv = document.createElement("div")
+        container.appendChild(newdiv);
+        newdiv.classList.add("quest")
+        let realquestioncontainer = document.createElement("p")
+        newdiv.appendChild(realquestioncontainer);
+        realquestioncontainer.innerHTML = questionshown.question
+        let answers = (questionsTot.correct_answer + (questionsTot.incorrect_answers[]))
+        for (let i=0; i<answers.length; i++) {
+            let inputs = document.createElement(("input type='radio'"))
+            realquestioncontainer.appendChild(inputs);            
+        }
+    }
+    let radios = document.querySelectorAll("input[type='radio]")
+    for (let i=0; i<radios.length; i++) {
+        let radioFinal = radios[i];
+        radios.addEventListener("change", check)
+    }
+}
+
+
+
+window.onload = function () {
+    showQuestions()
+}
